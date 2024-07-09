e3bebba1b506c0c7372d21a4f8378f40f5b789ba
JIHOON LEE <jihoon7705@gmail.com>
2024-07-09 16:56:02 +0900

    fix : eslint 오류 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
-import type { AppProps } from "next/app";
-import { Fragment } from "react";
-import { ThemeProvider } from "styled-components";
-import theme from "@/styles/theme";
-import GlobalStyled from "@/styles/GlobalStyled";
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
+import GlobalStyled from '@/styles/GlobalStyled'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Fragment>
-      <ThemeProvider theme={theme}>
-        <GlobalStyled />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Fragment>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyled />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }

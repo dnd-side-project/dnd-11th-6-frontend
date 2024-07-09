96816baecd2b678e5fc4c03704c757e2cb5c5f10
JIHOON LEE <jihoon7705@gmail.com>
2024-07-09 13:27:53 +0900

    chore : next-pwa 설치
    - next config 수정
    - manifest 파일 생성
    - metadata 적용
    - build & run
    - Lighthouse 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: "public",
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#9aa7f9" />
+      </Head>
       <body>
         <Main />
         <NextScript />

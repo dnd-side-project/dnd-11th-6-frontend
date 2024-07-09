4376469b5d323d2eaac98d90e7d46ddc9b05c116
JIHOON LEE <jihoon7705@gmail.com>
2024-07-09 16:50:47 +0900

    chore : prettier,Eslint 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
 import { ThemeProvider } from "styled-components";
-import theme from "../styles/theme";
+import theme from "@/styles/theme";
 import GlobalStyled from "@/styles/GlobalStyled";
 
 export default function App({ Component, pageProps }: AppProps) {

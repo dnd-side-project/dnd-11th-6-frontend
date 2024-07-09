6510962d953b3e1b6934b64d2ee215223cd5011e
JIHOON LEE <jihoon7705@gmail.com>
2024-07-09 14:05:55 +0900

    feat : styled-components global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { createGlobalStyle } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: white;
+  }
+  `;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }

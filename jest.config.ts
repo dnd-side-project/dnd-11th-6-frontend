19837d5ab007708aa71b59281f050398551ea66c
Dawon00 <dawon000203@gmail.com>
2024-07-11 18:57:05 +0900

    chore: Jest 및 관련 라이브러리 설치

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}

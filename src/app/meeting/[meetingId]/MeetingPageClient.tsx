2662a30bb410d2df460dc2c6e9aec4521da9cd90
Dawon00 <dawon000203@gmail.com>
2024-07-31 23:12:27 +0900

    refactor: meeting 의 데이터를 서버 컴포넌트에서 가져오도록 분리

---
+++ src/app/meeting/[meetingId]/MeetingPageClient.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { useEffect } from 'react'
+import useMeetingStore from '@/stores/useMeetingStore'
+
+interface Photo {
+  id: string
+  name: string
+}
+
+interface MeetingPageClientProps {
+  meetingId: string
+  photos: Photo[]
+}
+
+function MeetingPageClient({ meetingId, photos }: MeetingPageClientProps) {
+  const { setMeetingId } = useMeetingStore()
+
+  useEffect(() => {
+    setMeetingId(meetingId)
+  }, [meetingId, setMeetingId])
+
+  return (
+    <div className="flex flex-col">
+      <div>meeting: {meetingId}</div>
+      <div className="flex justify-between items-center mb-3">
+        <div className="text-xl">LOGO</div>
+        <div className="rounded-full w-12 h-12 bg-gray-300" />
+      </div>
+      <div className="flex justify-between bg-gray-300 mb-5">
+        <div>***님은 1장 중 1장 찍으셨어요</div>
+        <div>찍은사진 보기&gt;</div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex mb-5">
+          <input type="checkbox" />
+          <div>모임 미션 사진 보기</div>
+        </div>
+        <div>사진 삭제하기</div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {photos.map((photo) => (
+          <div
+            key={photo.id}
+            className="bg-pink-200 w-full h-52 cursor-pointer"
+          >
+            {photo.name}
+          </div>
+        ))}
+      </div>
+      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
+        <button
+          type="button"
+          className="w-52 h-12 bg-black text-white rounded-full"
+        >
+          📷 사진 찍기
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default MeetingPageClient
--- src/app/meeting/[meetingId]/page.tsx
+++ src/app/meeting/[meetingId]/page.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from 'react'
-import useMeetingStore from '@/stores/useMeetingStore'
+import MeetingPageClient from './MeetingPageClient'
 
-function MeetingPage({ params }: { params: { meetingId: string } }) {
-  const { meetingId, setMeetingId } = useMeetingStore()
-
-  useEffect(() => {
-    setMeetingId(params.meetingId)
-  }, [params.meetingId, setMeetingId])
-
-  const photos = [
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getPhotos(meetingId: string) {
+  return [
     { id: '1', name: 'photo1' },
     { id: '2', name: 'photo2' },
     { id: '3', name: 'photo3' },
@@ -20,44 +14,11 @@ function MeetingPage({ params }: { params: { meetingId: string } }) {
     { id: '9', name: 'photo9' },
     { id: '10', name: 'photo10' },
   ]
-  return (
-    <div className="flex flex-col">
-      <div>meeting: {meetingId}</div>
-      <div className="flex justify-between items-center mb-3">
-        <div className="text-xl">LOGO</div>
-        <div className="rounded-full w-12 h-12 bg-gray-300" />
-      </div>
-      <div className="flex justify-between bg-gray-300 mb-5">
-        <div>***님은 1장 중 1장 찍으셨어요</div>
-        <div>찍은사진 보기&gt;</div>
-      </div>
-      <div className="flex justify-between">
-        <div className="flex mb-5">
-          <input type="checkbox" />
-          <div>모임 미션 사진 보기</div>
-        </div>
-        <div>사진 삭제하기</div>
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        {photos.map((photo) => (
-          <div
-            key={photo.id}
-            className="bg-pink-200 w-full h-52 cursor-pointer"
-          >
-            {photo.name}
-          </div>
-        ))}
-      </div>
-      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
-        <button
-          type="button"
-          className="w-52 h-12 bg-black text-white rounded-full"
-        >
-          📷 사진 찍기
-        </button>
-      </div>
-    </div>
-  )
+}
+
+async function MeetingPage({ params }: { params: { meetingId: string } }) {
+  const photos = await getPhotos(params.meetingId)
+  return <MeetingPageClient meetingId={params.meetingId} photos={photos} />
 }
 
 export default MeetingPage

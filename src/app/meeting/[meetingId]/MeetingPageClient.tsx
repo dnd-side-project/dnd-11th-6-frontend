7609a7c2094b24ea07039e5d5c54ca4b424938da
Dawon00 <dawon000203@gmail.com>
2024-08-02 23:43:16 +0900

    feat: 모임 상세 정보 UI 구현

--- src/app/meeting/[meetingId]/MeetingPageClient.tsx
+++ src/app/meeting/[meetingId]/MeetingPageClient.tsx
@@ -1,47 +1,67 @@
 'use client'
 
+import { useState } from 'react'
+import Drawer from '@/components/Drawer/Drawer'
 import useMeetingStore from '@/stores/useMeetingStore'
+import MeetingInfo from './_components/MeetingInfo'
 
 function MeetingPageClient() {
   const { meetingId, photos } = useMeetingStore()
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+
+  const handleToggle = (): void => setIsVisible(!isVisible)
+  const handleClose = (): void => setIsVisible(false)
 
   return (
-    <div className="flex flex-col">
-      <div>meeting: {meetingId}</div>
-      <div className="flex justify-between items-center mb-3">
-        <div className="text-xl">LOGO</div>
-        <div className="rounded-full w-12 h-12 bg-gray-300" />
-      </div>
-      <div className="flex justify-between bg-gray-300 mb-5">
-        <div>***님은 1장 중 1장 찍으셨어요</div>
-        <div>찍은사진 보기&gt;</div>
-      </div>
-      <div className="flex justify-between">
-        <div className="flex mb-5">
-          <input type="checkbox" />
-          <div>모임 미션 사진 보기</div>
-        </div>
-        <div>사진 삭제하기</div>
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        {photos.map((photo) => (
+    <>
+      <div className="flex flex-col">
+        <div>meeting: {meetingId}</div>
+        <div className="flex justify-between items-center mb-3">
+          <div className="text-xl">LOGO</div>
           <div
-            key={photo.id}
-            className="bg-pink-200 w-full h-52 cursor-pointer"
+            onClick={handleToggle}
+            className="flex justify-center items-center rounded-full w-12 h-12 bg-gray-300"
+            tabIndex={0}
+            role="button"
+            onKeyDown={(e) => e.key === 'Enter' && handleToggle()}
           >
-            {photo.url}
+            photo
           </div>
-        ))}
+        </div>
+        <div className="flex justify-between bg-gray-300 mb-5">
+          <div>***님은 1장 중 1장 찍으셨어요</div>
+          <div>찍은사진 보기&gt;</div>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex mb-5">
+            <input type="checkbox" />
+            <div>모임 미션 사진 보기</div>
+          </div>
+          <div>사진 삭제하기</div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {photos.map((photo) => (
+            <div
+              key={photo.id}
+              className="bg-pink-200 w-full h-52 cursor-pointer"
+            >
+              {photo.url}
+            </div>
+          ))}
+        </div>
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
+          <button
+            type="button"
+            className="w-52 h-12 bg-black text-white rounded-full"
+          >
+            📷 사진 찍기
+          </button>
+        </div>
       </div>
-      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
-        <button
-          type="button"
-          className="w-52 h-12 bg-black text-white rounded-full"
-        >
-          📷 사진 찍기
-        </button>
-      </div>
-    </div>
+      <Drawer isVisible={isVisible} onClose={handleClose}>
+        <MeetingInfo />
+      </Drawer>
+    </>
   )
 }
 

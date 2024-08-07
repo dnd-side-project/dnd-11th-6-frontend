5189146d42bab5ac746934af8492c7ba6f2615af
Dawon00 <dawon000203@gmail.com>
2024-08-07 19:34:04 +0900

    fix: MeetingPageClient 컴포넌트 오류, 오타 수정

--- src/app/meeting/[meetingId]/MeetingPageClient.tsx
+++ src/app/meeting/[meetingId]/MeetingPageClient.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 import Drawer from '@/components/Drawer/Drawer'
 import useMeetingStore, { Photo } from '@/stores/useMeetingStore'
 import MeetingInfo from './_components/MeetingInfo'
-import Link from 'next/link'
-
 
 interface MeetingPageClientProps {
   initialMeetingId: string
@@ -24,47 +23,45 @@ function MeetingPageClient({
     setPhotos(initialPhotos)
   }, [initialMeetingId, initialPhotos])
 
-
   const { meetingId, photos } = useMeetingStore()
 
   const toggleDrawer = (): void => setIsVisible(!isVisible)
   const closeDrawer = (): void => setIsVisible(false)
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div>meeting: {meetingId}</div>
-        <div className="flex justify-between items-center mb-3">
-          <div className="text-xl">LOGO</div>
+    <div className="flex flex-col">
+      <div>meeting: {meetingId}</div>
+      <div className="flex justify-between items-center mb-3">
+        <div className="text-xl">LOGO</div>
+        <div
+          onClick={toggleDrawer}
+          className="flex justify-center items-center rounded-full w-12 h-12 bg-gray-300"
+          tabIndex={0}
+          role="button"
+          onKeyDown={(e) => e.key === 'Enter' && toggleDrawer()}
+        >
+          photo
+        </div>
+      </div>
+      <div className="flex justify-between bg-gray-300 mb-5">
+        <div>***님은 1장 중 1장 찍으셨어요</div>
+        <div>찍은사진 보기&gt;</div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex mb-5">
+          <input type="checkbox" id="mission-photo-checkbox" />
+          <label htmlFor="mission-photo-checkbox">모임 미션 사진 보기</label>
+        </div>
+        <div>사진 삭제하기</div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {photos.map((photo) => (
           <div
-            onClick={toggleDrawer}
-            className="flex justify-center items-center rounded-full w-12 h-12 bg-gray-300"
-            tabIndex={0}
-            role="button"
-            onKeyDown={(e) => e.key === 'Enter' && toggleDrawer()}
+            key={photo.id}
+            className="bg-pink-200 w-full h-52 cursor-pointer"
           >
-            photo
+            {photo.url}
           </div>
-        </div>
-        <div className="flex justify-between bg-gray-300 mb-5">
-          <div>***님은 1장 중 1장 찍으셨어요</div>
-          <div>찍은사진 보기&gt;</div>
-        </div>
-        <div className="flex justify-between">
-          <div className="flex mb-5">
-            <input type="checkbox" id="misson-photo-checkbox" />
-            <label htmlFor="misson-photo-checkbox">모임 미션 사진 보기</label>
-          </div>
-          <div>사진 삭제하기</div>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          {photos.map((photo) => (
-            <div
-              key={photo.id}
-              className="bg-pink-200 w-full h-52 cursor-pointer"
-            >
-              {photo.url}
-            </div>
         ))}
       </div>
       <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
@@ -73,7 +70,6 @@ function MeetingPageClient({
             type="button"
             className="w-52 h-12 bg-black text-white rounded-full"
           >
-
             📷 사진 찍기 {meetingId}
           </button>
         </Link>
@@ -81,7 +77,7 @@ function MeetingPageClient({
       <Drawer isVisible={isVisible} onClose={closeDrawer}>
         <MeetingInfo />
       </Drawer>
-    </>
+    </div>
   )
 }
 

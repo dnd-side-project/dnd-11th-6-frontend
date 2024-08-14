8bac15e61479ee468d23b4348c5f14c6ce2a7ccd
JIHOON LEE <jihoon7705@gmail.com>
2024-08-14 23:26:06 +0900

    feat : section 1 기능, UI 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRef } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Home() {
   const section2Ref = useRef<HTMLElement>(null)
@@ -21,28 +22,60 @@ function Home() {
             height={50}
             className="m-auto"
           />
-          <h1 className="text-4xl font-bold my-4">Snappy</h1>
+          <h1 className="text-4xl font-bold my-2">Snappy</h1>
           <p className="mt-2 font-normal text-lg text-gray-700">
             즐거운 모임의 순간들을 <br /> 스냅피와 포착해보세요
           </p>
-          <div className="mt-8 grid grid-cols-2 gap-4">
-            <button className="bg-gray-200 p-4 rounded-lg transition duration-300 hover:bg-gray-300">
-              내 모임 앨범
-              <br />
-              참여하기
-            </button>
-            <button className="bg-gray-200 p-4 rounded-lg transition duration-300 hover:bg-gray-300">
-              새로운 모임 앨범
-              <br />
-              만들기
-            </button>
+          <div className="mt-16 grid grid-cols-2 gap-4">
+            <Link
+              href="/create-meeting"
+              className="flex items-center justify-center flex-col bg-gray-900 text-white p-9 rounded-2xl transition duration-300 "
+            >
+              <Image
+                className="mb-3"
+                src="./icons/plus.svg"
+                alt="Plus Icon"
+                width={44}
+                height={44}
+              />
+              <span>
+                새로운 모임 앨범
+                <br />
+                만들기
+              </span>
+            </Link>
+            <Link
+              href="/entry-meeting"
+              className="flex items-center justify-center flex-col bg-gray-900 text-white p-9 rounded-2xl transition duration-300 "
+            >
+              <Image
+                className="mb-3"
+                src="./icons/snap.svg"
+                alt="Snap Icon"
+                width={61.85}
+                height={45.58}
+              />
+              <span className="text-white">
+                내 모임 앨범
+                <br />
+                참여하기
+              </span>
+            </Link>
           </div>
           <button
             onClick={scrollToSection2}
-            className="mt-24 m-auto flex flex-col items-center transition duration-300 hover:text-blue-500 cursor-pointer animate-move-up-down"
+            className="mt-20 m-auto flex flex-col items-center transition duration-300 cursor-pointer animate-move-up-down"
           >
-            <p className="mb-2">눌러서 설명보기</p>
-            <div className="animate-bounce">↓</div>
+            <p className="mb-2 font-bold text-gray-700 text-lg">
+              눌러서 설명보기
+            </p>
+            <Image
+              className="mb-3"
+              src="./icons/double-down.svg"
+              alt="DoubleDown Icon"
+              width={24}
+              height={24}
+            />
           </button>
         </div>
       </section>

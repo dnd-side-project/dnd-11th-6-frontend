f0b5557af66129de70ff06c207ec9fbf33cff120
JIHOON LEE <jihoon7705@gmail.com>
2024-08-14 23:40:15 +0900

    feat : section 3 디자인 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,17 +98,17 @@ function Home() {
         <Image src="/image1.svg" alt="Flags Icon" width={800} height={800} />
       </section>
       {/* Section 3 */}
-      <section className="snap-start h-screen flex flex-col items-center justify-center bg-black text-white">
-        <div className="text-left opacity-0 transform translate-y-4 transition duration-1000 ease-in-out animate-fadeIn">
+      <section className=" h-screen flex flex-col items-center justify-center bg-gray-900 text-white snap-start">
+        <div className="w-full px-4 text-left opacity-0 transform translate-y-4 transition duration-1000 ease-in-out animate-fadeIn">
           <h2 className="text-sm font-medium mb-2 text-yellow-400">모임앨범</h2>
-          <p className="mb-2 text-xl font-bold">
+          <p className="text-[22px] font-bold line leading-[30px] mb-[6px]">
             우리 모임만의
             <br />
             프라이빗한 앨범을 만들 수 있어요
           </p>
           <p className="text-sm text-gray-400">자유롭게 사진을 찍고 공유해요</p>
         </div>
-        <Image src="/image2.svg" alt="Flags Icon" width={500} height={500} />
+        <Image src="/image2.svg" alt="Flags Icon" width={400} height={400} />
       </section>
 
       {/* Section 4 */}

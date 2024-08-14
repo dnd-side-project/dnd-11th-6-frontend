97d9858c7d7a756eccbdfd129ad12762927f6f72
Dawon00 <dawon000203@gmail.com>
2024-08-14 15:42:05 +0900

    feat: Callout 컴포넌트 구현 및 storybook 추가

---
+++ src/components/Callout/index.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta } from '@storybook/react'
+import Callout from './index'
+
+export default {
+  title: 'Components/Callout',
+  component: Callout,
+  argTypes: {
+    onButtonClick: { action: 'clicked' },
+  },
+} as Meta<typeof Callout>
+
+function Template(args: React.ComponentProps<typeof Callout>) {
+  return <Callout {...args} />
+}
+export function Default(args: React.ComponentProps<typeof Callout>) {
+  return <Template {...args} />
+}
+Default.args = {
+  title: '모임 링크가 없으신가요?',
+  content:
+    '안전한 모임 앨범을 위해 링크가 없으면 입장이 어려워요.<br/> 링크가 없다면 나만의 모임 앨범을 새로 만들어보세요.',
+
+  buttonText: '새로운 모임 앨범 만들기',
+  onButtonClick: () => {},
+}

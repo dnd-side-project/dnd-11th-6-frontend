1f31f008605e397abd17a631f45b470590704919
Dawon00 <dawon000203@gmail.com>
2024-08-15 20:24:12 +0900

    style: Input 컴포넌트 높이 설정

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -44,7 +44,7 @@ export function Input<T extends FieldValues>({
   checkingMessage = '확인중',
   description,
 }: InputProps<T>) {
-  const inputClassName = `w-full py-4 px-[18px] border rounded-[14px] text-[18px] focus:border-gray-900 focus:outline-none focus:ring-0 ${className} ${error ? 'border-red-500 focus:border-red-500' : 'border-gray-500'}`
+  const inputClassName = `w-full h-[54px] py-4 px-[18px] border rounded-[14px] text-[18px] focus:border-gray-900 focus:outline-none focus:ring-0 ${className} ${error ? 'border-red-500 focus:border-red-500' : 'border-gray-500'}`
 
   const renderInput = (field: any) => {
     const props = {

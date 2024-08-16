37bf47a386bd007237aa116aabb5a19ce44825ac
JIHOON LEE <jihoon7705@gmail.com>
2024-08-17 01:13:17 +0900

    feat : meetingDate 스키마 구조 변경
    - Meeting Form 구조 변경
    - 스키마 데이터 분리 및 유효성 검사 코드 추가

--- src/app/create-meeting/_components/MeetingDate.tsx
+++ src/app/create-meeting/_components/MeetingDate.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import dayjs from 'dayjs'
+import Image from 'next/image'
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
 import useMeetingForm from '../_hooks/useMeetingForm'
@@ -9,12 +11,13 @@ function MeetingDate() {
     control,
     formState: { errors, isValid },
     handleSubmit,
+    getValues,
   } = meetingDateForm
 
   return (
     <div className="flex flex-col h-full">
       <div className="mb-[42px]">
-        <h2 className="text-xl font-bold mb-2">모임은 언제 진행되나요? </h2>
+        <h2 className="text-xl font-bold mb-2">모임은 언제 진행되나요?</h2>
         <p className="text-sm text-gray-600">
           모임이 진행되는 날짜와 시간을 알려주세요.
         </p>
@@ -41,8 +44,23 @@ function MeetingDate() {
             as="datetime"
             error={errors.endDate?.message}
           />
+          <div className="flex bg-gray-50 py-3 px-4 rounded">
+            <Image
+              src="../icons/alert.svg"
+              className="mr-2"
+              width={20}
+              height={20}
+              alt="alert"
+            />
+            <div className=" flex flex-col text-sm font-medium">
+              <p>모임 사진 촬영은 모임 기간 중에만 가능해요.</p>
+              <p>모임에 맞게 종료일을 선택해주세요.</p>
+            </div>
+          </div>
         </div>
-
+        <span className="bg-gray-50 text-sm font-semibold mb-3 p-[10px] rounded text-center text-gray-700">
+          {`링크는 ${dayjs(getValues('endDate').toString()).format('YYYY-MM-DD')} 까지 유효합니다.`}
+        </span>
         <Button type="submit" disabled={!isValid} fullWidth variant="primary">
           다음
         </Button>

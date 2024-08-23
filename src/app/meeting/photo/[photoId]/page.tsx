f4ed5b705dd4b9d90c23d2b2be7845010a894013
Dawon00 <dawon000203@gmail.com>
2024-08-24 00:04:32 +0900

    feat: 스냅 상세 보기 페이지 구현

---
+++ src/app/meeting/photo/[photoId]/page.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useGetSnapDetail } from '@/apis/queries/snapQueries'
+import Close from '@/assets/close.svg'
+import { Button } from '@/components/Button'
+import useMeetingStore from '@/stores/useMeetingStore'
+import Dice from 'public/icons/dice.svg'
+import Download from 'public/icons/download.svg'
+import formatCaptureTime from './formatCaptureTime'
+
+function PhotoDetail({ params }: { params: { photoId: string } }) {
+  const router = useRouter()
+  const meetingId = useMeetingStore().meetingData?.meetingId
+  const {
+    data: snapData,
+    isLoading,
+    isError,
+  } = useGetSnapDetail(meetingId || 0, Number(params.photoId))
+
+  const handleDownload = async () => {
+    if (!snapData) return
+    try {
+      const imageUrl = `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${snapData.data.snapUrl}`
+      const response = await fetch(imageUrl)
+      const blob = await response.blob()
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = url
+      link.download = `Snappy_${snapData.data.shootDate}.jpg`
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+      window.URL.revokeObjectURL(url)
+    } catch (error) {
+      console.error('Download failed:', error)
+    }
+  }
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error loading snap details</div>
+  if (!snapData) return <div>No data available</div>
+
+  const { snapUrl, shootDate, type, photographer, mission } = snapData.data
+  const fullImageUrl = `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${snapUrl}`
+
+  return (
+    <div className="flex flex-col items-center w-full min-h-screen ">
+      <div className="flex justify-between items-center h-12 w-full p-4">
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-body1-semibold text-gray-900">
+            {photographer.nickname}님의 스냅
+          </div>
+        </div>
+        <button onClick={() => router.back()} className="cursor-pointer">
+          <Image src={Close} alt="Close Button" className="w-4 h-4" />
+        </button>
+      </div>
+
+      <div className="mt-6">
+        {mission?.content && (
+          <div className=" rounded-[14px] px-[18px] py-[10px] bg-[#F2F5F5] text-body2 text-gray-900">
+            {mission.content}
+          </div>
+        )}
+      </div>
+
+      <div className="w-full mt-auto mb-auto">
+        <div className="relative w-full pb-[100%] overflow-hidden rounded-[14px]">
+          <Image
+            src={fullImageUrl}
+            alt="찍은 사진"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div className="flex justify-center items-center mt-3">
+          {mission?.content && (
+            <div className="flex items-center bg-gray-100 rounded-[14px] px-[10px] py-1 text-caption text-gray-900 mr-3">
+              <Image src={Dice} alt="Dice Icon" className="w-4 h-4 mr-1" />
+              {type === 'RANDOM_MISSION' ? '랜덤미션' : '모임미션'}
+            </div>
+          )}
+          <div className="text-label text-gray-600">
+            {formatCaptureTime(shootDate)}
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full mt-auto mb-5 p-4">
+        <Button
+          onClick={handleDownload}
+          type="button"
+          variant="primary"
+          className="text-white text-body1-semibold w-full relative bg-point-mint"
+        >
+          <Image
+            src={Download}
+            alt="Download Button"
+            className="w-4 h-4 mr-2"
+          />
+          사진 다운받기
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default PhotoDetail

db06fc5d9b910ce7a0c651231d087ad6ae23114b
JIHOON LEE <jihoon7705@gmail.com>
2024-08-27 00:38:12 +0900

    chore : jest 설정 변경

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,16 @@
-export default {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
+
+export default config

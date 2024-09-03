0c4205d4620a448d3806a4e145ed1728e01165f9
JIHOON LEE <jihoon7705@gmail.com>
2024-09-03 19:57:16 +0900

    fix : checkout storybook refactor
    - https://github.com/dnd-side-project/dnd-11th-6-frontend/pull/88#discussion_r1733014352

--- src/components/Inputs/CheckboxInput/index.stories.tsx
+++ src/components/Inputs/CheckboxInput/index.stories.tsx
@@ -1,24 +1,28 @@
-import React, { ComponentProps } from 'react'
+import React from 'react'
 import { useForm } from 'react-hook-form'
-import { StoryFn, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import { CheckboxInput } from './index'
 
-export default {
+const meta: Meta<typeof CheckboxInput> = {
   title: 'Components/Inputs/CheckboxInput',
   component: CheckboxInput,
   argTypes: {
     name: { control: 'text' },
     label: { control: 'text' },
   },
-} as Meta<typeof CheckboxInput>
-
-const Template: StoryFn<ComponentProps<typeof CheckboxInput>> = (args) => {
-  const { control } = useForm()
-  return <CheckboxInput {...args} control={control} />
 }
 
-export const Default = Template.bind({})
-Default.args = {
-  name: 'default',
-  label: 'Accept terms and conditions',
+export default meta
+
+type Story = StoryObj<typeof CheckboxInput>
+
+export const Default: Story = {
+  render: (args) => {
+    const { control } = useForm()
+    return <CheckboxInput {...args} control={control} />
+  },
+  args: {
+    name: 'default',
+    label: 'Accept terms and conditions',
+  },
 }

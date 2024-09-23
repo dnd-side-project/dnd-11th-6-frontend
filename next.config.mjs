905031a826c84d1d809a59ef3914663ae8839862
JIHOON LEE <jihoon7705@gmail.com>
2024-09-23 20:10:35 +0900

    fix : 도메인 주소 상수값으로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import withPWA from 'next-pwa'
+import { API_BASE_URL, IMAGE_BASE_URL } from '@/constant/base_url'
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,10 +10,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://dnd-11th-6.s3.ap-northeast-2.amazonaws.com'.replace(
-          /^https?:\/\//,
-          '',
-        ),
+        hostname: IMAGE_BASE_URL.replace(/^https?:\/\//, ''),
         port: '',
       },
     ],
@@ -21,7 +19,7 @@ const nextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'https://api.get-snappy.co.kr/api/v1/:path*',
+        destination: `${API_BASE_URL}/:path*`,
       },
     ]
   },

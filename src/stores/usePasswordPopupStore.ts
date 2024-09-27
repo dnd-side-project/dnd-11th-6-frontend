97f034b664413f3ec83753bdfbecf13c2eaf5f5d
Dawon00 <dawon000203@gmail.com>
2024-09-27 16:37:57 +0900

    feat: closePopupAndRedirect 별도 구현

--- src/stores/usePasswordPopupStore.ts
+++ src/stores/usePasswordPopupStore.ts
@@ -4,24 +4,22 @@ type PasswordPopupStore = {
   isOpen: boolean
   meetingId: number | null
   onConfirm: ((password: string) => void) | null
-  onClose: (() => void) | null
-  openPopup: (
-    meetingId: number,
-    onConfirm: (password: string) => void,
-    onClose: () => void,
-  ) => void
+  openPopup: (meetingId: number, onConfirm: (password: string) => void) => void
   closePopup: () => void
+  closePopupAndRedirect: () => void
 }
 
 export const usePasswordPopupStore = create<PasswordPopupStore>((set) => ({
   isOpen: false,
   meetingId: null,
   onConfirm: null,
-  onClose: null,
-  openPopup: (meetingId, onConfirm, onClose) =>
-    set({ isOpen: true, meetingId, onConfirm, onClose }),
-  closePopup: () =>
-    set({ isOpen: false, meetingId: null, onConfirm: null, onClose: null }),
+  openPopup: (meetingId, onConfirm) =>
+    set({ isOpen: true, meetingId, onConfirm }),
+  closePopup: () => set({ isOpen: false, meetingId: null, onConfirm: null }),
+  closePopupAndRedirect: () => {
+    set({ isOpen: false, meetingId: null, onConfirm: null })
+    window.location.href = '/'
+  },
 }))
 
 export default usePasswordPopupStore

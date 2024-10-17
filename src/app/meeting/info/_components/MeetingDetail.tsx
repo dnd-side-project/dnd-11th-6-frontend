55a5cc8a19585e5190dd9f3872f7eb32e740ab0c
Dawon00 <dawon000203@gmail.com>
2024-10-18 00:01:53 +0900

    refactor: 모임 시작, 종료 일시 포맷팅

--- src/app/meeting/info/_components/MeetingDetail.tsx
+++ src/app/meeting/info/_components/MeetingDetail.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
+import dayjs from 'dayjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useParticipants } from '@/apis/queries/participantsQueries'
@@ -14,6 +15,11 @@ function MeetingDetail() {
   const [showToggle, setShowToggle] = useState(false)
   const description = meetingData?.description ?? ''
   const MAX_LENGTH = 80
+
+  const formatDate = (dateString: string | undefined) => {
+    if (!dateString) return '-'
+    return dayjs(dateString).format('YYYY.MM.DD A hh:mm')
+  }
 
   const limit = 10
   const {
@@ -68,17 +74,16 @@ function MeetingDetail() {
         </div>
         <div className="flex mt-4">
           <div className="flex flex-col w-1/2 bg-gray-50 rounded-[14px] p-3">
-            {' '}
             <div className="text-label-semibold text-gray-700">시작</div>
             <div className="text-caption mt-1 text-gray-500">
-              {meetingData?.startDate}
+              {formatDate(meetingData?.startDate)}
             </div>
           </div>
           <div className="w-3" />
           <div className="flex flex-col w-1/2 bg-gray-50 rounded-[14px] p-3">
             <div className="text-label-semibold text-gray-700">종료</div>
             <div className="text-caption mt-1 text-gray-500">
-              {meetingData?.endDate}
+              {formatDate(meetingData?.endDate)}
             </div>
           </div>
         </div>
